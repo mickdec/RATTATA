0b87cf82a85e5f53627d12026c7b713574ce14b9
Mickaël Decelle <mickael.decelle@ca-gip.fr>
2023-11-08 11:47:09 +0100

    Error patch, using getaddrinfo.

--- CL/Windows/main.c
+++ CL/Windows/main.c
@@ -121,6 +121,7 @@ void init_socket()
     int recvbuflen = DEFAULT_BUFLEN;
 
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
